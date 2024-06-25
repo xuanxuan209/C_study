bcd2cc16e8eed3ba83d5959a74b0d4e4a30132d9
xuanxuan209 <1694968493@qq.com>
2024-06-25 20:30:30 +0800

    2024年第一次提交代码到git上

---
+++ test_3_31/test_3_31/源.cpp
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
+//c语言中一定要有main函数
+#include <stdio.h>
+
+//int main() {
+//	printf("hello world");
+//	printf("%zu\n", sizeof(char));
+//	printf("%zu", sizeof(int));
+//	printf("%zu", sizeof(long));
+//	printf("%zu", sizeof(short));
+//	printf("%zu", sizeof(long long));
+//	printf("%zu", sizeof(float));
+//	printf("%zu", sizeof(double));
+//	return 0;
+//}
+//
+//int main() {
+//	int a = 100;
+//	printf("a=%d", a);
+//	return 0;
+//	
+//}
+
+//int main() {
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d %d", &a, &b);
+//	int c = a + b;
+//	printf("%d", c);
+//	return 0;
+//
+//}
+extern int a;
+int main() {
+	printf("a=%d\n", a);
+}
